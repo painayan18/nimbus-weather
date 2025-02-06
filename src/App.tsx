26e23e31f3022ee37e815f89b4bfddcd3d45f831
nayanpai <nayanspai@gmail.com>
2025-02-06 16:57:58 -0600

    initial setup and shadcn components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+    <div>React App
+    <Button variant="destructive">Counter</Button>
+    </div>
+  )
+}
+
+export default App
