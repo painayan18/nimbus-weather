5ee5289ed9e8ec5e3d432cf9d26cff39bf2e7ebf
nayanpai <nayanspai@gmail.com>
2025-02-07 19:31:17 -0600

    Theme toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
-import { Link, Moon, Sun } from "lucide-react";
+import { Link } from "lucide-react";
 import { useTheme } from "@/context/theme-provider.tsx";
 import CitySearch from "@/components/city-search.tsx";
+import { ThemeToggle } from "@/components/theme-toggle.tsx";
 
 const Header = () => {
-  const { theme, setTheme } = useTheme();
-  const isDark = theme === "dark";
+  const { theme } = useTheme();
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
@@ -20,17 +20,7 @@ const Header = () => {
         <div className="flex gap-4">
           {/*    Search */}
           <CitySearch />
-
-          <div
-            onClick={() => setTheme(isDark ? "light" : "dark")}
-            className={`flex items-center cursor-pointer transition-transform duration-500 ${isDark ? "rotate-180" : "rotate-0"}`}
-          >
-            {isDark ? (
-              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
-            ) : (
-              <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
-            )}
-          </div>
+          <ThemeToggle />
         </div>
       </div>
     </header>
